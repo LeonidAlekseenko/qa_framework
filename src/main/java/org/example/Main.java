be20b45893d376a55aa9c908a3e05ed98758c321
leoni <bvqsdd@gmail.com>
2023-01-28 03:45:06 +0300

    install selenium......

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("hello");
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
